6262e86d95e8b325c49159ed9926cd84dfaa27a2
unknown <suhailali3454@gmail.com>
2023-07-16 10:03:00 +0530

    final commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
+
 public class RWOnly {
-    
+    private String name;
+
+    public String getN() {
+        return name;
+    }
+
+    public void setN(String name) {
+        this.name = name;
+
+    }
 }
+
+
